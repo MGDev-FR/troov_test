352626aa593bf28b04c41cb0e8418826c25671b6
Michaël Godinho Da Costa <contact@michael-godinhodacosta.fr>
2022-07-22 09:30:24 -0400

    Change typo

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -8,11 +8,11 @@
             <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav v-if="$auth.loggedIn">
                 Username
-                <b-nav-item @click="$auth.logout()">Logout</b-nav-item>
+                <b-nav-item @click="$auth.logout()">Déconnexion</b-nav-item>
             </b-navbar-nav>
             <b-navbar-nav v-else>
-                <b-nav-item to="/login">Login</b-nav-item>
-                <b-nav-item to="/register">Register</b-nav-item>
+                <b-nav-item to="/login">Connexion</b-nav-item>
+                <b-nav-item to="/register">Inscription</b-nav-item>
             </b-navbar-nav>
             </b-collapse>
         </b-navbar>
